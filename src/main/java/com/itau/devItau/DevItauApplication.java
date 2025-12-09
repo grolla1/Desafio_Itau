c838ea9e86ba733b1dec063dfdae95043691add0
grolla1 <mateusggrolla@gmail.com>
2025-12-09 00:11:45 -0300

    Criando repositório

---
+++ src/main/java/com/itau/devItau/DevItauApplication.java
@@ -0,0 +1,13 @@
+package com.itau.devItau;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevItauApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevItauApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/itau/devItau/DevItauApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itau.devItau;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevItauApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
