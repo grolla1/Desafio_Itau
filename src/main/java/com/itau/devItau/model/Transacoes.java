f816f76405703af659bb6b88ada740a387227a5f
grolla1 <mateusggrolla@gmail.com>
2026-01-19 23:22:34 -0300

    Criando model da arquitetura

--- src/main/java/com/itau/devItau/model/Transacoes.java
+++ src/main/java/com/itau/devItau/model/Transacoes.java
@@ -1,8 +1,40 @@
 package com.itau.devItau.model;
 
-public class Model {
-    private float valor;
-    private String dataHora;
+import jakarta.validation.constraints.NotNull;
 
-    
+import java.time.OffsetDateTime;
+import java.util.UUID;
+
+public class Transacoes {
+    //apesar de não persistir as informações no banco, anotação para garantir a completude da classe
+    @NotNull
+    private UUID id;
+    @NotNull
+    private Float valor;
+    @NotNull
+    private OffsetDateTime dataHora;
+
+    public UUID getId() {
+        return this.id;
+    }
+
+    public Float getValor() {
+        return this.valor;
+    }
+
+    public OffsetDateTime getDataHora() {
+        return this.dataHora;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public void setValor(Float valor) {
+        this.valor = valor;
+    }
+
+    public void setDataHora(OffsetDateTime dataHora) {
+        this.dataHora = dataHora;
+    }
 }
