1fdb42d6d025bb6fbf9d4706886a5d4a3c770395
grolla1 <mateusggrolla@gmail.com>
2026-01-21 00:33:47 -0300

    Criando metodo para buscar os dados estatísticos das transações nos últimos segundos. Criado DTO response para organizar melhor a arquitetura.

--- src/main/java/com/itau/devItau/dto/TransacoesResponse.java
+++ src/main/java/com/itau/devItau/dto/TransacoesResponse.java
@@ -1,5 +1,60 @@
 package com.itau.devItau.dto;
 
-public class Response {
-    
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+public class TransacoesResponse {
+    private long count;
+
+    private BigDecimal sum;
+
+    private BigDecimal avg;
+
+    private BigDecimal min;
+
+    private BigDecimal max;
+
+    //Getters
+    public BigDecimal getAvg() {
+        return avg;
+    }
+
+    public BigDecimal getMax() {
+        return max;
+    }
+
+    public BigDecimal getMin() {
+        return min;
+    }
+
+    public BigDecimal getSum() {
+        return sum;
+    }
+
+    public long getCount() {
+        return count;
+    }
+
+    //Setters
+    public void setAvg(BigDecimal avg) {
+        this.avg = avg;
+    }
+
+    public void setCount(long count) {
+        this.count = count;
+    }
+
+    public void setMax(BigDecimal max) {
+        this.max = max;
+    }
+
+    public void setMin(BigDecimal min) {
+        this.min = min;
+    }
+
+    public void setSum(BigDecimal sum) {
+        this.sum = sum;
+    }
 }
+
--- src/main/java/com/itau/devItau/service/TransacoesService.java
+++ src/main/java/com/itau/devItau/service/TransacoesService.java
@@ -1,4 +1,79 @@
 package com.itau.devItau.service;
 
-public class Service {
+import com.itau.devItau.dto.TransacoesResponse;
+import com.itau.devItau.model.TransacoesModel;
+import org.springframework.stereotype.Service;
+import com.itau.devItau.repository.TransacoesRep;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class TransacoesService {
+    private final TransacoesRep repository;
+
+    public TransacoesService(TransacoesRep repository) {
+        this.repository = repository;
+    }
+
+    public TransacoesResponse getTransactionsStatistics(long seconds) {
+        List<TransacoesModel> lastSecondsTransactions = new ArrayList<>();
+
+        Instant cutOff = Instant.now().minusSeconds(seconds);
+
+        lastSecondsTransactions = repository.findTransactionsAfter(cutOff);
+
+        TransacoesResponse response = new TransacoesResponse();
+
+        response.setCount(lastSecondsTransactions.size());
+        response.setMax(findMaxValue(lastSecondsTransactions));
+        response.setMin(findMinValue(lastSecondsTransactions));
+        response.setAvg(defineAvg(lastSecondsTransactions));
+        response.setSum(sumValues(lastSecondsTransactions));
+
+        return response;
+    }
+
+    private BigDecimal findMaxValue(List<TransacoesModel> transacoes) {
+        return transacoes.stream()
+                .map(TransacoesModel::getValor)
+                .max(BigDecimal::compareTo)
+                .orElse(BigDecimal.ZERO);
+    }
+
+    private BigDecimal findMinValue(List<TransacoesModel> transacoes) {
+        return transacoes.stream()
+                .map(TransacoesModel::getValor)
+                .min(BigDecimal::compareTo)
+                .orElse(BigDecimal.ZERO);
+    }
+
+    private BigDecimal defineAvg(List<TransacoesModel> transacoes) {
+        if (transacoes.isEmpty()) {
+            return BigDecimal.ZERO;
+        }
+
+        BigDecimal soma = BigDecimal.ZERO;
+        for (TransacoesModel transacao : transacoes) {
+            soma = soma.add(transacao.getValor());
+        }
+
+        return soma.divide(BigDecimal.valueOf(transacoes.size()), 2, RoundingMode.HALF_EVEN);
+    }
+
+    private BigDecimal sumValues(List<TransacoesModel> transacoes) {
+        if (transacoes.isEmpty()) {
+            return BigDecimal.ZERO;
+        }
+
+        BigDecimal soma = BigDecimal.ZERO;
+        for (TransacoesModel transacao : transacoes) {
+            soma = soma.add(transacao.getValor());
+        }
+
+        return soma;
+    }
 }
