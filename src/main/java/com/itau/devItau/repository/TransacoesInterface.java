e6d9b4b3a36515dac362e94fd26de10a60c9fb4d
grolla1 <mateusggrolla@gmail.com>
2026-01-22 00:51:41 -0300

    Incluindo anotação no repository

--- src/main/java/com/itau/devItau/repository/TransacoesInterface.java
+++ src/main/java/com/itau/devItau/repository/TransacoesInterface.java
@@ -1,12 +1,14 @@
 package com.itau.devItau.repository;
 
 import com.itau.devItau.model.TransacoesModel;
+import org.springframework.stereotype.Repository;
 
 import java.time.Instant;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
+@Repository
 public interface TransacoesInterface {
 
     TransacoesModel save(TransacoesModel transacao);
