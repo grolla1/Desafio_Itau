9f528b0eb140147cce846e141877e4171542d50a
grolla1 <mateusggrolla@gmail.com>
2026-01-22 00:53:44 -0300

    Fit: ErrorResponse para estruturar a mensagem de erro

--- src/main/java/com/itau/devItau/dto/ErrorResponse.java
+++ src/main/java/com/itau/devItau/dto/ErrorResponse.java
@@ -1,4 +1,18 @@
 package com.itau.devItau.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.OffsetDateTime;
+
+@Getter
+@Setter
 public class ErrorResponse {
+    private String messageEx;
+
+    OffsetDateTime timeStamp = OffsetDateTime.now();
+
+    public ErrorResponse(String messageEx) {
+        this.messageEx = messageEx;
+    }
 }
