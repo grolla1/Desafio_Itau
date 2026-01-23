4f10985d2d0ebb9012d269ffa482b2e394da647e
grolla1 <mateusggrolla@gmail.com>
2026-01-22 23:06:00 -0300

    fit: incluindo tratativa de erro para parametro faltando

--- src/main/java/com/itau/devItau/exception/GlobalExceptionHandler.java
+++ src/main/java/com/itau/devItau/exception/GlobalExceptionHandler.java
@@ -2,7 +2,7 @@ package com.itau.devItau.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -23,5 +23,15 @@ public class GlobalExceptionHandler {
         return ResponseEntity
                 .status(HttpStatus.UNPROCESSABLE_ENTITY)
                 .body(new ErrorResponse(exception.getMessage()));
+    }
+
+    @ExceptionHandler(MissingServletRequestParameterException.class)
+    public ResponseEntity<ErrorResponse> handleTransactionMissingParamException(MissingServletRequestParameterException exception) {
+
+        String message = "O parâmetro '" + exception.getParameterName() + "' é obrigatório.";
+
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(new ErrorResponse(message));
     }
 }
